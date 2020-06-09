12dcf8016dee0cb1e7e8d2fb67da97330e891f79
SpecCRA <benjamin.y.xiao@gmail.com>
2020-06-09 05:34:14 -0700

    adding data parse script

---
+++ data_parsing.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import json
+import sys
+import os
+
+print('Python: {}'.format(sys.version))
+print('pandas: {}'.format(pd.__version__))
+print('NumPy: {}'.format(np.__version__))
+
+games_path = 'data_files/Video_Games.json'
+
+data = list()
+
+with open(games_path) as f:
+    for line in f:
+        data.append(json.loads(line.strip()))
+
+games_df = pd.DataFrame(data)
+# Retain rating, text review, and ID of product
+export_df = games_df[['overall', 'reviewText', 'asin']]
+
+print('Rows exported: {}'.format(len(export_df)))
+
+export_df.to_pickle('games_data.pkl')
